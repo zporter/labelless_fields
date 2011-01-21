e7d9e43ef563041ede9ba14033ffced5a7b73449
Zachary Porter <zporter9@gmail.com>
2011-01-21 10:02:40 -0500

    added an example to test off of and successfully looping through all labels within the given form tag

--- src/jquery.labelless.js
+++ src/jquery.labelless.js
@@ -1,11 +1,26 @@
-(function( $ ){
-  $.fn.labelless = function() {
+(function( $ ) {
+	
+  $.fn.labelless = function( options ) {
+	
+		var settings = {
+			'fadeOnFocus': true,
+			'fadeOpacity': .7,
+			'fadeDuration': 500
+		};
   
     return this.each( function() {
-		var $this = $(this);
+			var $this = $(this);
+			
+			// if options exist, merge with default settings
+			if ( options ) {
+				$.extend( settings, options );
+			};
 		
-		// do stuff
-	});
+			// loop through all labels in the given form tag
+			$this.find('label').each( function() {
+				console.dir($(this));
+			});
+		});
 
   };
 })( jQuery );
